fd3ccd24fa08086146d7dab26a80356bf70bf1c4
eadietz <emmanuelle.dietz@airbus.com>
2021-10-20 16:04:11 +0200

    fixed a windows path name issue

--- run_simulation.py
+++ run_simulation.py
@@ -8,8 +8,11 @@ class Simulation():
 
     def __init__(self, json_bool=True):
 
-        self.path_sep = "\\" if _platform.startswith('win') else "/"
-        self.abs_path = f'{os.path.dirname(__file__)}' if _platform.startswith('win') else f'{os.path.dirname(__file__)}/'
+        self.path_sep = "/"
+        self.abs_path = f'{os.path.dirname(__file__)}/'
+
+        # for windows ...
+        self.abs_path = self.abs_path[1:] if self.abs_path.startswith("C:") else self.abs_path
 
         self.model_components_path = f'{self.abs_path}benchmarks{self.path_sep}model-components{self.path_sep}'
         self.config_file = 'meta_info.csv'
