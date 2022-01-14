b9ae7d4106c4ca6e39053f428a4ad2609997d60a
eadietz <emmanuelle.dietz@airbus.com>
2022-01-14 16:10:34 +0100

    rewrote analysis.py

--- benchmarks/use-cases/flight-task/analysis/analysis.py
+++ benchmarks/use-cases/flight-task/analysis/analysis.py
@@ -22,7 +22,6 @@ import random
 
 class Analysis:
 
-
     def __init__(self, path):
 
         self.rmse_collector = 0
@@ -32,9 +31,77 @@ class Analysis:
         self.sep = ";"
         self.compared_df = pd.DataFrame()
         self.idx = 0
+
+        self.server_url = f"ws://localhost:2048/fsuipc/"
+        self.websocket = client.connect(self.server_url, subprotocols=['fsuipc'])
+        self.counter = 0
+        print("* Start running main task")
+        try:
+            print('* run eventloop')
+            asyncio.run(self.eventloop())
+            print('* close eventloop')
+        except:
+            print('* The Connection could not be established')
+
+    async def eventloop(self):
+
+        async with client.connect(self.server_url, subprotocols=['fsuipc']) as self.websocket:
+            print("* Websocket connected")
+
+            offsets_declare = {
+                "command": 'offsets.declare',
+                "name": 'OffsetsWrite',
+                "offsets": [
+                    {"name": 'write', "address": 0x66D0, "type": 'int', "size": 4},
+                ]
+            }
+            print(offsets_declare)
+            await self.websocket.send(json.dumps(offsets_declare))
+
+            primary_response_data = await self.websocket.recv()
+            print(json.loads(primary_response_data))
+
+            if json.loads(primary_response_data).get("success"):
+                print(*"Offsets Declared Successful")
+
+            while json.loads(primary_response_data).get("success"):
+                await self.write()
+            else:
+                print(json.loads(primary_response_data).get("success", "* No Success Message has been returned"))
+                print("[ERROR] Offsets have not been declared correctly")
+                print("[ERROR] Error Message", json.loads(primary_response_data).get("errorMessage"), "Error Code",
+                      json.loads(primary_response_data).get("errorCode"))
+
+    async def write(self, value):
+        print("write function called, value is", value)
+
+        n = random.randint(0, 365)
+        offsets_write_dict = {
+            "command": 'offsets.write',
+            "name": 'OffsetsWrite',
+            "offsets": [
+                {"name": 'write', "value": n}
+            ]
+        }
+        await self.websocket.send(json.dumps(offsets_write_dict))
+        primary_response_data2 = await self.websocket.recv()
+        print(primary_response_data2)
+        primary_response_data2_json = json.loads(primary_response_data2)
+        primary_response_data2_data = primary_response_data2_json.get("data")
+
+        if primary_response_data2_data == None:
+            self.counter = self.counter + 1
+
+            print("* ERROR: NO DATA RETURNED")
+            if self.counter == 3:
+                print(*"NO DATA HAS BEEN RETURED AFTER WRITING OFFSET")
+
+        else:
+            primary_response_data2_value = primary_response_data2_data.get("write")
+            self.counter = 0
+            print(f"Offset written with value {primary_response_data2_value}")
+
     # called by the cognitive model in act-r to compute similarity between two numbers
-
-
     def numberSimilarities(self, a, b):
         if isinstance(b, numbers.Number) and isinstance(a, numbers.Number):
             # similarity based on euclidean distance
