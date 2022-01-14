36c18f13f3b9752bf0507c681326fbb7d1c2314e
AVIENG <95969199+AVIENG@users.noreply.github.com>
2022-01-14 13:13:54 +0100

    Update server_based-writer.py
    Server Based Writer umgeschrieben, Schreibfuntion unabhängig vom eventloop

--- txt2actr/environment2actr/server_based-writer.py
+++ txt2actr/environment2actr/server_based-writer.py
@@ -5,11 +5,43 @@ from websockets import client
 import json
 import sys
 import random
+server_url = f"ws://localhost:2048/fsuipc/"
+websocket = client.connect(server_url, subprotocols=['fsuipc'])
+counter = 0
+   
+
+async def write():
+        global counter            
+        n = random.randint(0,365)
+        offsets_write_dict = {
+            "command": 'offsets.write',
+            "name": 'OffsetsWrite',
+            "offsets": [
+                { "name": 'write', "value": n}                                                                                                                                                                                                                                                                                                                                                                                      
+                ]
+                }
+        await websocket.send(json.dumps(offsets_write_dict))
+        primary_response_data2=await websocket.recv()
+        print(primary_response_data2)
+        primary_response_data2_json = json.loads(primary_response_data2)
+        primary_response_data2_data = primary_response_data2_json.get("data")
+        
+        if primary_response_data2_data == None:
+            counter = counter + 1
+
+            print("* ERROR: NO DATA RETURNED")
+            if counter == 3:
+                print(*"NO DATA HAS BEEN RETURED AFTER WRITING OFFSET")
+                
+        else:
+            primary_response_data2_value = primary_response_data2_data.get("write")
+            counter=0
+            print(f"Offset written with value {primary_response_data2_value}")
 
 
 async def eventloop():
+        global websocket
 
-        server_url = f"ws://localhost:2048/fsuipc/"
         async with client.connect(server_url, subprotocols=['fsuipc']) as websocket:
             print("* Websocket connected")
 
@@ -20,43 +52,17 @@ async def eventloop():
                     { "name": 'write', "address": 0x66D0, "type": 'int', "size": 4 },                                                                                                                                                                                                                                                                                                                                                                                      
                 ]
                 }
-
+            print(offsets_declare)
             await websocket.send(json.dumps(offsets_declare))
 
             primary_response_data=await websocket.recv()
             print(json.loads(primary_response_data))
-
+            
             if json.loads(primary_response_data).get("success"):
-                print(*"Offsets Declared Successful")
+                print(*"Offsets Declared Successful")                
 
-
-                while True: 
-                
-                    n = random.randint(0,365)
-                    offsets_write_dict = {
-                        "command": 'offsets.write',
-                        "name": 'OffsetsWrite',
-                        "offsets": [
-                            { "name": 'write', "value": n}                                                                                                                                                                                                                                                                                                                                                                                      
-                            ]
-                            }
-                    await websocket.send(json.dumps(offsets_write_dict))
-                    primary_response_data2=await websocket.recv()
-                    print(primary_response_data2)
-                    primary_response_data2_json = json.loads(primary_response_data2)
-                    primary_response_data2_data = primary_response_data2_json.get("data")
-                    
-                    if primary_response_data2_data == None:
-                        counter = counter + 1
-
-                        print("* ERROR: NO DATA RETURNED")
-                        if counter == 3:
-                            print(*"NO DATA HAS BEEN RETURED AFTER WRITING OFFSET")
-                            break
-                    else:
-                        primary_response_data2_value = primary_response_data2_data.get("write")
-                        counter=0
-                        print(f"Offset written with value {primary_response_data2_value}")
+            while json.loads(primary_response_data).get("success"):
+                await write()
 
 
             else:  
