ceb5b66acb456d5ce1e4888e78a3b315b3d5c92a
eadietz <emmanuelle.dietz@airbus.com>
2022-01-17 15:08:55 +0100

    fixed merge in analysis.py

--- benchmarks/use-cases/flight-task/analysis/analysis.py
+++ benchmarks/use-cases/flight-task/analysis/analysis.py
@@ -124,66 +124,7 @@ class Analysis:
             print('* close write')
         except:
             print('* The Connection could not be established')
-
-
-
-    async def eventloop(self):
-
-        server_url = f"ws://localhost:2048/fsuipc/"
-        async with client.connect(server_url, subprotocols=['fsuipc']) as websocket:
-            print("* Websocket connected")
-
-            offsets_declare = {
-                "command": 'offsets.declare',
-                "name": 'OffsetsWrite',
-                "offsets": [
-                    {"name": 'write', "address": 0x66D0, "type": 'int', "size": 4},
-                ]
-            }
-
-            await websocket.send(json.dumps(offsets_declare))
-
-            primary_response_data = await websocket.recv()
-            print(json.loads(primary_response_data))
-
-            if json.loads(primary_response_data).get("success"):
-                print(*"Offsets Declared Successful")
-
-                while True:
-
-                    n = random.randint(0, 365)
-                    offsets_write_dict = {
-                        "command": 'offsets.write',
-                        "name": 'OffsetsWrite',
-                        "offsets": [
-                            {"name": 'write', "value": n}
-                        ]
-                    }
-                    await websocket.send(json.dumps(offsets_write_dict))
-                    primary_response_data2 = await websocket.recv()
-                    print(primary_response_data2)
-                    primary_response_data2_json = json.loads(primary_response_data2)
-                    primary_response_data2_data = primary_response_data2_json.get("data")
-
-                    if primary_response_data2_data == None:
-                        counter = counter + 1
-
-                        print("* ERROR: NO DATA RETURNED")
-                        if counter == 3:
-                            print(*"NO DATA HAS BEEN RETURED AFTER WRITING OFFSET")
-                            break
-                    else:
-                        primary_response_data2_value = primary_response_data2_data.get("write")
-                        counter = 0
-                        print(f"Offset written with value {primary_response_data2_value}")
-
-
-            else:
-                print(json.loads(primary_response_data).get("success", "* No Success Message has been returned"))
-                print("[ERROR] Offsets have not been declared correctly")
-                print("[ERROR] Error Message", json.loads(primary_response_data).get("errorMessage"), "Error Code",
-                      json.loads(primary_response_data).get("errorCode"))
-
+            
     # write results into file
     def reset(self, log_file=None):
         # write back old data to file before reseting variables
